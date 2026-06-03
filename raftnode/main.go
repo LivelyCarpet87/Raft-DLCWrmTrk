5eb0e3e900b73646c0ec0f7b7e8dbfc6c52f0986
Tyllis Xu <LivelyCarpet87@gmail.com>
2026-06-03 17:50:23 -0500

    Use command line flags
    Transition to command line flags.
    Begin using command types

--- raftnode/main.go
+++ raftnode/main.go
@@ -6,14 +6,18 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
-	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"strings"
 	"time"
+	"flag"
+	"fmt"
 
 	"raft-dlcwrmtrk/fsm"
 	"raft-dlcwrmtrk/raftnode"
+	"raft-dlcwrmtrk/raftcommands"
 
 	"github.com/hashicorp/raft"
 	"github.com/hashicorp/go-hclog"
@@ -21,18 +25,11 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-type Command struct {
-	Op   string        `json:"op"`
-	Args []interface{} `json:"args"`
-}
-
-type Envelope struct {
-	Cmd Command `json:"cmd"`
-}
-
 type JoinRequest struct {
 	ID       string `json:"id"`
+	FailureDomain string `json:"failureDomain"`
 	RaftAddr string `json:"raft_addr"`
+	GrpcAddr string `json:"grpc_addr"`
 	HttpAddr string `json:"http_addr"`
 }
 
@@ -78,49 +75,102 @@ func joinCluster(leader string, self JoinRequest) error {
 }
 
 func main() {
-
-	id := os.Args[1]
-	raftAddr := os.Args[2]
-	httpAddr := os.Args[3]
-	bootstrap := false
-	leader := ""
-
 	rootLogger := hclog.New(&hclog.LoggerOptions{
 		Name:  "cluster",
 		Level: hclog.Debug,
 	})
+	log := rootLogger.Named("main")
 
-	if (os.Args[4] == "bootstrap") {
-		bootstrap = true
-	} else if (os.Args[4] == "join") {
-		leader = os.Args[5]
-	} else {
-		seeds := strings.Split(os.Args[4], ",")
-		leader_found, err := discoverLeader(seeds)
+	nodeID := 			flag.String("nodeID", "", "The ID for this node. Each node in the cluster must have a distinct ID.")
+	failureDomain := 	flag.String("fd", "", "The failure domain for this node. Nodes with the same listed failure domain should be more likely to fail together.")
+	ip_raft := 			flag.String("addr.raft", "0.0.0.0", "The IP address the raft node listens at.")
+	ip_grpc := 			flag.String("addr.grpc", "", "The IP address the grpc server listens at. Defaults to the value for addr.raft")
+	ip_http := 			flag.String("addr.http", "", "The IP address the http server listens at. Defaults to the value for addr.raft")
+	port_raft := 		flag.Int("port.raft", 6000, "The RAFT port for this node. Used for RAFT coordination within the cluster. Defaults to 6000.")
+	port_grpc := 		flag.Int("port.grpc", -1, "The gRPC port for this node. Used for gRPC calls within the cluster. Defaults to port.raft+1000.")
+	port_http := 		flag.Int("port.http", -1, "The http port for this node. Used by the HTTP server for the web API. Defaults to port.raft+2000.")
+	bootstrap :=		flag.Bool("bootstrap", false, "Bootstrap when starting the cluster for the first time. ONLY 1 node can be bootstrapped.")
+	peersList :=		flag.String("peers", "", "A comma separated list of peers in the cluster. Ex. addr1:grpc_port1,addr2:grpc_port2")
+
+	flag.Parse()
+	if (*nodeID == ""){
+		flag.Usage()
+		panic("nodeID is required.")
+	} else if (*failureDomain == ""){
+		flag.Usage()
+		panic("failure domain is required.")
+	} else if (*port_raft < 1 || *port_raft > 65535){
+		flag.Usage()
+		panic("port.raft must be between 1 and 65535.")
+	} else if (!*bootstrap && *peersList == ""){
+		flag.Usage()
+		panic("A peer list must be provided when not bootstrapping.")
+	}
+
+	if (*ip_grpc == ""){
+		ip_grpc = ip_raft
+		log.Info("Using default value", "gRPC_IP",*ip_grpc,)
+	}
+	if (*ip_http == ""){
+		ip_http = ip_raft
+		log.Info("Using default value", "HTTP_IP",*ip_http)
+	}
+
+	log.Info("Using","RAFT port",*port_raft,)
+	if (*port_grpc == -1){
+		*port_grpc = *port_raft + 1000
+		log.Info("Using default value","gRPC_port",*port_grpc)
+	}
+	if (*port_grpc < 1 || *port_grpc > 65535){
+		flag.Usage()
+		panic("port.grpc must be between 1 and 65535.")
+	}
+
+	if (*port_http == -1){
+		*port_http = *port_raft + 2000
+		log.Info("Using default value","HTTP_port",*port_http)
+	}
+	if (*port_http < 1 || *port_http > 65535){
+		flag.Usage()
+		panic("port.http must be between 1 and 65535.")
+	}
+
+	leader := ""
+	peers := strings.Split(*peersList, ",")
+	raftAddr := fmt.Sprintf("%s:%d", *ip_raft, *port_raft)
+	grpcAddr := fmt.Sprintf("%s:%d", *ip_grpc, *port_grpc)
+	httpAddr := fmt.Sprintf("%s:%d", *ip_http, *port_http)
+
+
+	if (!*bootstrap) {
+		leader_found, err := discoverLeader(peers)
 		if err != nil {
-			log.Fatal(err)
+			log.Error("Failed to find leader", "err",err)
 		}
 		leader = leader_found
+		log.Info("Found leader", "leader",leader)
 	}
 
-	db, _ := sql.Open("sqlite", id+".db")
+	db, _ := sql.Open("sqlite", *nodeID+".db")
 	fsm.InitSchema(db)
 
-	node, err := raftnode.NewNode(id, raftAddr, httpAddr, db, rootLogger, bootstrap)
+	node, err := raftnode.NewNode(*nodeID, raftAddr, *failureDomain, grpcAddr, httpAddr, db, rootLogger, *bootstrap)
 	if err != nil {
-		log.Fatal(err)
+		log.Error("Failed to create RAFT node", "err",err)
 	}
 
-	if !bootstrap {
+	if !*bootstrap {
 		time.Sleep(500 * time.Millisecond) // small safety delay
 	
 		selfJoinReq := JoinRequest{
-			ID:       id,
+			ID:       *nodeID,
+			FailureDomain: *failureDomain,
 			RaftAddr: raftAddr,
+			GrpcAddr: grpcAddr,
 			HttpAddr: httpAddr,
 		}
 		if err := joinCluster(leader, selfJoinReq); err != nil {
-			log.Fatal(err)
+			log.Error("Failed to join cluster", "err",err)
 		}
 	}
 
@@ -142,25 +192,6 @@ func main() {
 		w.Write([]byte(httpAddr))
 	})
 
-	// ---- EXEC (KV ops) ----
-	http.HandleFunc("/exec", func(w http.ResponseWriter, r *http.Request) {
-
-		if node.Raft.State() != raft.Leader {
-			http.Error(w, "not leader", 403)
-			return
-		}
-
-		body, _ := io.ReadAll(r.Body)
-
-		f := node.Raft.Apply(body, 5*time.Second)
-		if err := f.Error(); err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-
-		w.Write([]byte("ok"))
-	})
-
 	// ---- CLUSTER JOIN ----
 	http.HandleFunc("/cluster/join", func(w http.ResponseWriter, r *http.Request) {
 
@@ -172,20 +203,23 @@ func main() {
 		var req JoinRequest
 		json.NewDecoder(r.Body).Decode(&req)
 
-		cmd := Envelope{
-			Cmd: Command{
-				Op: "add_node",
-				Args: []interface{}{
-					req.ID,
-					req.RaftAddr,
-					req.HttpAddr,
-				},
-			},
+		addNodeCommand := raftcommands.AddNodeCommand{
+			NodeUID: req.ID,
+			FailureDomain: req.FailureDomain,
+			RaftAddr: req.RaftAddr,
+			GrpcAddr: req.GrpcAddr,
+			HttpAddr: req.HttpAddr,
+		}
+		cmdData, _ := json.Marshal(addNodeCommand)
+
+		cmdEnv := raftcommands.CommandEnvelope{
+			Command: "AddNode",
+			Data: cmdData,
 		}
 
-		data, _ := json.Marshal(cmd)
+		cmdEnvData, _ := json.Marshal(cmdEnv)
 
-		if err := node.Raft.Apply(data, 5*time.Second).Error(); err != nil {
+		if err := node.Raft.Apply(cmdEnvData, 5*time.Second).Error(); err != nil {
 			http.Error(w, err.Error(), 500)
 			return
 		}
@@ -198,6 +232,11 @@ func main() {
 		w.Write([]byte("joined"))
 	})
 
-	log.Println("http on", httpAddr)
-	log.Fatal(http.ListenAndServe(httpAddr, nil))
+	log.Info("Starting HTTP server", "httpAddr",httpAddr)
+	go http.ListenAndServe(httpAddr, nil)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	<-sig
 }
